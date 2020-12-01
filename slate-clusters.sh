97e287152579568dacf4fc83ecba9f46a6eb71ae
Gabriele Carcassi <carcassi@umich.edu>
2020-12-01 10:51:47 -0500

    Adding certificate check to master script

--- slate-clusters.sh
+++ slate-clusters.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd "$( dirname "$(readlink -f "${BASH_SOURCE[0]}" )" )"
 ./for-all.sh ../kconf_extraction/configs ./slate-cluster-availability-check.sh
+./for-all.sh ../kconf_extraction/configs ./slate-cluster-certificate-check.sh
