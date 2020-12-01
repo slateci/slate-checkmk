9ae27fffea3041bdd07fe2bdd0b89119eb01d609
Gabriele Carcassi <carcassi@umich.edu>
2020-12-01 10:45:13 -0500

    Adding check for cluster certificate

---
+++ slate-cluster-certificate-check.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Help message
+if [ "$#" -ne 2 ]; then
+    echo "Checks whether the certificate of a cluster is about to expire"
+    echo
+    echo "      $0 KUBECONF CLUSTERNAME"
+    echo
+    echo "Example:  $0 ~/.kube/conf mycluster"
+    exit -1
+fi
+
+
+kubectlconfig=$1
+clustername=$2
+
+IP=$(cat $kubectlconfig 2>/dev/null | grep server | awk '{print $2}' | cut -d'/' -f3)
+ENDDATE=$(timeout 1 openssl s_client -showcerts -connect "$IP" 2>/dev/null | openssl x509 -noout -enddate 2>/dev/null | cut -d'=' -f2)
+if [ $(date -d "$ENDDATE" +%s) -gt $(date -d "now + 90 days" +%s) ]; then
+    echo 0 SLATE-cluster-$clustername-certificate - Certificate is valid and willl expire in more than 90 days: $IP \($ENDDATE\)
+else
+    echo 2 SLATE-cluster-$clustername-certificate - Certificate is invalid or needs to be renewed: $IP \($ENDDATE\)
+fi
