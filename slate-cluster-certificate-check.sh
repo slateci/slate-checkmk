979e43512fc17fe9a25183e56806a8c5d41a184b
Gabriele Carcassi <carcassi@umich.edu>
2020-12-09 09:58:39 -0500

    Certificate check: warn in 60 days and crit in 15 days from expiration

--- slate-cluster-certificate-check.sh
+++ slate-cluster-certificate-check.sh
@@ -16,8 +16,12 @@ clustername=$2
 
 IP=$(cat $kubectlconfig 2>/dev/null | grep server | awk '{print $2}' | cut -d'/' -f3)
 ENDDATE=$(timeout 1 openssl s_client -showcerts -connect "$IP" 2>/dev/null | openssl x509 -noout -enddate 2>/dev/null | cut -d'=' -f2)
-if [ $(date -d "$ENDDATE" +%s) -gt $(date -d "now + 90 days" +%s) ]; then
-    echo 0 SLATE-cluster-$clustername-certificate - Certificate is valid and willl expire in more than 90 days: $IP \($ENDDATE\)
+if [ $(date -d "$ENDDATE" +%s) -gt $(date -d "now + 60 days" +%s) ]; then
+    echo 0 SLATE-cluster-$clustername-certificate - Certificate is valid and willl expire in more than 60 days: $IP \($ENDDATE\)
 else
+  if [ $(date -d "$ENDDATE" +%s) -gt $(date -d "now + 15 days" +%s) ]; then
+    echo 1 SLATE-cluster-$clustername-certificate - Certificate is valid and willl expire in less than 60 days: $IP \($ENDDATE\)
+  else
     echo 2 SLATE-cluster-$clustername-certificate - Certificate is invalid or needs to be renewed: $IP \($ENDDATE\)
+  fi
 fi
